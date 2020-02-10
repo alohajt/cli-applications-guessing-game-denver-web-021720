768adf84e61018f57982b97f32300cfa3bacd061
JT Lu <alohajt94@gmail.com>
2020-02-10 21:48:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,9 +2,9 @@
 def run_guessing_game
   #puts "Guess a number between 1 and 6."
   number = rand(1..6)
-  input = gets.chomp.to_s
+  input = gets.chomp
 
-  if input == number
+  if input.to_i == number
     puts "You guessed the correct number!"
     elsif input == 'exit'
     puts "Goodbye!"
