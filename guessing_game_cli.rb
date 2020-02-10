91c911f5003929cd4ada4b7715cdf8285cfe58d3
JT Lu <alohajt94@gmail.com>
2020-02-10 21:20:59 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,7 +9,7 @@ def run_guessing_game
     elsif input == 'exit'
     puts "Goodbye!"
   else
-    puts "Sorry! The computer guessed #{number+1}."
+    puts "Sorry! The computer guessed #{number}."
   end
 
 end
